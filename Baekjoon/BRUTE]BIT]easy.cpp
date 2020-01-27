c3e6a40a238317d6cee72bdd0ae9594ca674695a
Jiun Kim <jkim2@bowdoin.edu>
2020-01-27 05:12:48 -0500

    baekjoon
    baekjoon

--- Baekjoon/BRUTE]BIT]easy.cpp
+++ Baekjoon/BRUTE]BIT]easy.cpp
@@ -1,5 +1,5 @@
-//baekjoon 13460
-//marble escape
+//baekjoon 12100
+//easy
 
 #include <iostream>
 #include <string>
@@ -27,74 +27,109 @@ vector<int> generate(int k){
 bool check_range(int y, int x){
 	return y>=0 && y<n && x>=0 && x<n;
 }
-bool simulate(vector<vector<pair<int,bool>>> &b, int dir){	
-	int dy = newy[dir];
-	int dx = newx[dir];
-	bool moved = false;
-	for(int i = 0; i<n;i++){
-		for(int j = 0;j<n;j++){
-			if(b[i][j].first == 0) continue;
-			bool blocked = false;
-			bool merge = false;
-			while(!blocked){
-				int cur = b[i][j].first;
-				int ni = i + dy;
-				int nj = j + dx;
-				// cout<<i<<" "<<j<<"\n";
-				// cout<<ni<<" "<<nj<<"\n";
-				if(!check_range(ni, nj)){
-					break;	
-				} 
-				
-				int next = b[ni][nj].first;
-				if(next == cur){
-					if(b[ni][nj].second || b[i][j].second){
-						blocked = true;
-					}
-					else{
-						b[ni][nj].first *= 2;
-						b[i][j].first = 0;
-						b[ni][nj].second = true;
-						blocked = true;
-						moved = true;
-					}
-				}
-				else if(next == 0){
-					int temp = b[ni][nj].first;
-					b[ni][nj].first = b[i][j].first;
-					b[i][j].first = temp;
-					i = ni;	j = nj;
-					blocked = true;
-					moved = true;
-				}
-				else if(next != cur){
-					blocked = true;
-				}
-			}
+bool simulate(vector<vector<pair<int,bool>>> &b, int dir, int i, int j){	
+	int cur = b[i][j].first;
+	int ni = i + newy[dir];
+	int nj = j + newx[dir];
+	if(!check_range(ni, nj)){
+		return false;	
+	} 
+	int next = b[ni][nj].first;
+	if(next == cur){
+		if(b[ni][nj].second || b[i][j].second){
+			return false;
+		}
+		else{
+			b[ni][nj].first *= 2;
+			b[i][j].first = 0;
+			b[ni][nj].second = true;
+			return true;
 		}
 	}
-	return moved;
+	else if(next == 0){
+		int temp = b[ni][nj].first;
+		b[ni][nj].first = b[i][j].first;
+		//second까지 바꿔주는게 중요함.
+		b[ni][nj].second = b[i][j].second;
+		b[i][j].first = 0;
+		return true;
+	}
+	return false;
 }
 int go(vector<int> dir, vector<vector<int>> &b){
-	int cnt = 0;
 	vector<vector<pair<int, bool>>> newb(n, vector<pair<int,bool>>(n));
 	for(int i = 0;i<n;i++){
 		for(int j = 0;j<n;j++){
 			newb[i][j].first = b[i][j];
 		}
 	}
+
 	for(int k:dir){
-		cnt++;
 		for(int i = 0;i<n;i++){
 			for(int j = 0;j<n;j++){
 				newb[i][j].second = false;
 			}
 		}
 		bool moved = true;
-		
+		while(moved){
+			moved = false;
+			if(k == 0){
+				for(int i = n-1;i>=0;i--){
+					for(int j = 0;j<n;j++){
+						if(newb[i][j].first == 0) continue;
+						if(simulate(newb, k, i, j)){
+							moved = true;
+						}
+					}
+				}	
+			}
+			if(k == 1){
+				for(int i = 0;i<n;i++){
+					for(int j = 0;j<n;j++){
+						if(newb[i][j].first == 0) continue;
+						if(simulate(newb, k, i, j)){
+							moved = true;
+						}
+					}
+				}	
+			}
+			if(k == 2){
+				for(int j = n-2;j>=0;j--){
+					for(int i = 0;i<n;i++){
+						if(newb[i][j].first == 0) continue;
+						if(simulate(newb, k, i, j)){
+							moved = true;
+						}
+					}
+				}	
+			}
+			if(k == 3){
+				for(int j = 1;j<n;j++){
+					for(int i = 0;i<n;i++){
+						if(newb[i][j].first == 0) continue;
+						if(simulate(newb, k, i, j)){
+							moved = true;
+						}
+					}
+				}	
+			}
+		}
+		// for(int i = 0;i<n;i++){
+		// 	for(int j = 0;j<n;j++){
+		// 		cout<<newb[i][j].first<<" ";
+		// 	}
+		// cout<<"\n";
+		// }
 	}
-
-	return cnt;
+	int ans = -1;
+	for(int i = 0;i<n;i++){
+		for(int j = 0;j<n;j++){
+			if(ans == -1 || ans<newb[i][j].first){
+				ans = newb[i][j].first;
+			}
+		}
+	}	
+	return ans;
 }
 
 
@@ -104,27 +139,19 @@ int main() {
 	for(int i = 0;i<n;i++){
 		for(int j = 0;j<n;j++){
 			cin>>blocks[i][j];	
-			cout<<blocks[i][j]<<" ";
+			// cout<<blocks[i][j]<<" ";
 		}
-		cout<<"\n";
+		// cout<<"\n";
 	}
+	vector<int> dir;
 	int ans = -1;
 	for(int i = 0;i<(1<<10);i++){
 		vector<int> dir = generate(i);
+		vector<vector<int>> newb = blocks;
+		int result = go(dir, newb);
+		if(ans == -1 || result>ans) ans = result;
 	}
-	vector<vector<pair<int, bool>>> newblock(n, vector<pair<int,bool>>(n));
-	for(int i = 0;i<n;i++){
-		for(int j = 0;j<n;j++){
-			newblock[i][j].first = blocks[i][j];
-		}
-	}
-	cout<<simulate(newblock, 3)<<"\n";
-	for(int i = 0;i<n;i++){
-		for(int j = 0;j<n;j++){
-			cout<<newblock[i][j].first<<" ";
-		}
-		cout<<"\n";
-	}	
-
+	cout<<ans<<"\n";
+	
     return 0;
 }
