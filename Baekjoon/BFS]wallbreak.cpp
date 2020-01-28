eb0698ba0ee1b10bb10d41a8110454c49870d1ff
Jiun Kim <jkim2@bowdoin.edu>
2020-01-28 09:31:11 -0500

    baekjoon
    baekjoon

--- Baekjoon/BFS]wallbreak.cpp
+++ Baekjoon/BFS]wallbreak.cpp
@@ -1,14 +1,13 @@
-//baekjoon 2206
-//벽 부수고 이동하기
+// Example program
 #include <iostream>
 #include <string>
 #include <vector>
 #include <queue>
+#include <tuple>
 using namespace std;
 
 int n,m;
 int check[1001][1001][2];
-vector<string> walls;
 int block[1001][1001];
 int dist[1001][1001][2];
 int dy[4] = {-1, 1, 0, 0};
@@ -18,49 +17,36 @@ bool check_range(int y, int x){
     return y>=0 && y<n && x>=0 && x<m;
 }
 bool go(int a, int b){
-    queue<pair<int,int>> q;
-    q.push(make_pair(a,b));
+    queue<tuple<int, int,int>> q;
+    q.push(make_tuple(a,b, 0));
     check[a][b][0] = 1;
-    dist[a][b][0] = 1;
     while(!q.empty()){
-        auto cur = q.front();
+        int y, x, z;
+        tie(y, x, z) = q.front();
         q.pop();
-        int y = cur.first;
-        int x = cur.second;
-        int d = 0;
-        int used = 1;//0 if not broken wall before
+        int d = check[y][x][z];
         if(y== n-1 && x == m-1) return true;
-        if(check[y][x][0]){
-            d = dist[y][x][0]; 
-            used = 0;
-        }
-        else{
-            d= dist[y][x][1];
-        }
         for(int i = 0;i<4;i++){
             int ny = y+ dy[i];
             int nx = x +dx[i];
             if(!check_range(ny, nx)){
                 continue;
             }
-            if(check[ny][nx][used]) continue;
+            if(check[ny][nx][z]>0) continue;
             if(block[ny][nx] == 1){
-                if(used == 0){
-                    q.push(make_pair(ny,nx));
-                    check[ny][nx][1] = 1;
-                    dist[ny][nx][1] = d+1;
+                if(z == 0){
+                    q.push(make_tuple(ny,nx,1));
+                    check[ny][nx][1] = d+1;
                 }
             }
             else if(block[ny][nx] == 0){
-                if(!used){
-                    q.push(make_pair(ny,nx));
-                    check[ny][nx][0] = 1;
-                    dist[ny][nx][0] = d+1;
+                if(!z){
+                    q.push(make_tuple(ny,nx,0));
+                    check[ny][nx][0] = d+1;
                 }
                 else{
-                    q.push(make_pair(ny,nx));
-                    check[ny][nx][1] = 1;
-                    dist[ny][nx][1] = d+1;
+                    q.push(make_tuple(ny,nx,1));
+                    check[ny][nx][1] = d+1;
                 }
             }
         }
@@ -74,28 +60,22 @@ int main(){
     cin>>n>>m;
     cin.ignore();
     for(int i = 0;i<n;i++){
-        string temp;
-        getline(cin, temp);
-        walls.push_back(temp);
         for(int j = 0;j<m;j++){
-            block[i][j] = walls[i].at(j) - '0';
+            scanf("%1d", &block[i][j]);  
+            
         }
+        
     }
     
-    // for(int i = 0;i<n;i++){
-    //     for(int j = 0;j<m;j++){
-    //         cout<<check[i][j][1]<<" ";
-    //     }
-    //     cout<<"\n";
-    // }
     if(go(0,0)){
-        if(check[n-1][m-1][0]) cout<<dist[n-1][m-1][0];
+        if(check[n-1][m-1][0]>0) cout<<check[n-1][m-1][0]<<"\n";
         else{
-            cout<<dist[n-1][m-1][1];
+            cout<<check[n-1][m-1][1]<<"\n";
         }
     }
     else{
         cout<<-1<<"\n";
     }
+ 
     return 0;
 }
