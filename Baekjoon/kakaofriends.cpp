576c2654597fb9288adad377a21bc6e8e18f522c
Jiun Kim <jkim2@bowdoin.edu>
2020-02-01 23:32:51 -0500

    baekjoon
    baekjoon

--- Baekjoon/kakaofriends.cpp
+++ Baekjoon/kakaofriends.cpp
@@ -1,3 +1,5 @@
+//kakaofriends
+//https://www.acmicpc.net/status?user_id=jiun9605&problem_id=15954&from_mine=1
 #include <iostream>
 #include <string>
 #include <vector>
@@ -18,30 +20,30 @@ int main(void){
         cumu[i] = pop[i];
         if(i!=0) cumu[i] += cumu[i-1];
     }
-    int l =k;
     double least = 10000000.0;
-    // for(int l =k;l<=n;l++){
-    for(int i = 0;i<=n-l;i++){
-        // cout<<i<<"\n";
-        double mean = 0.0;
-        if(i ==0) mean = (double)cumu[i+l-1];
-        else{
-            mean = (double)(cumu[i+l-1]-cumu[l-1]);    
-        }
-        mean /=(double)k;
-        double sq = 0.0;
-        for(int j = 0;j<l;j++){
-            sq += pow(((double)pop[i+j])-mean, (double)2.0);
-        }
-        // printf("%lf\n", sq/(double)l);
-        double variance = sqrt(sq/(double)l);
-        // cout<<variance<<"\n";
-        if(least>variance){
-            least = variance;
-        }
-    }    
-    // }
+    for(int l =k;l<=n;l++){
+        for(int i = 0;i<=n-l;i++){
+            // cout<<i<<"\n";
+            double mean = 0.0;
+            if(i ==0) mean = (double)cumu[i+l-1];
+            else{
+                mean = (double)(cumu[i+l-1]-cumu[i-1]);    
+            }
+            mean /=(double)l;
+            double sq = 0.0;
+            for(int j = 0;j<l;j++){
+                sq += pow(((double)pop[i+j])-mean, (double)2.0);
+            }
+            // printf("%lf\n", sq/(double)l);
+            double variance = sqrt(sq/(double)l);
+            // cout<<variance<<"\n";
+            if(least>variance){
+                least = variance;
+            }
+        }    
+    }
   
     printf("%.11lf\n", least);
     
 }
+
