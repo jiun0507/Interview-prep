52348f1d7e43682ef486682d1eee9f0797cde2c7
Jiun Kim <jkim2@bowdoin.edu>
2020-02-19 10:51:12 -0500

    baek
    beak

--- Baekjoon/BRUTE]chickendelivery.cpp
+++ Baekjoon/BRUTE]chickendelivery.cpp
@@ -9,15 +9,6 @@ vector<pair<int,int>> houses;
 vector<pair<int,int>> chickens;
 int town[50][50];
 
-vector<int> makevec(int bit){
-    vector<int> ans;
-    for(int i = 0;i<chickens.size();i++){
-        ans.push_back(bit&1);
-        bit >>=1;
-    }
-    return ans;
-}
-
 int main(void){
    cin>>n>>m;
    for(int i = 0;i<n;i++){
@@ -31,12 +22,19 @@ int main(void){
            }
        }
    }
+   vector<int> d(chickens.size());
+   for(int i = 0;i<m;i++){
+       d.push_back(1);
+   }
+   for(int i = 0;i<chickens.size()-m;i++){
+       d.push_back(0);
+   }
+   sort(d.begin(),d.end());
    int ans = -1;
-   for(int i = 0;i<(1<<chickens.size());i++){
-       vector<int> keep = makevec(i);
+   do{
        int cnt = 0;
         for(int j = 0;j<chickens.size();j++){ 
-            if(keep[j]==1) cnt++;
+            if(d[j]==1) cnt++;
         }
         if(cnt!=m) continue;
         int chickendistance = 0;
@@ -45,7 +43,7 @@ int main(void){
             int housey = houses[j].first;
             int housex = houses[j].second;
             for(int k = 0;k<chickens.size();k++){
-                if(keep[k] == 1){
+                if(d[k] == 1){
                     int dist = abs(housey-chickens[k].first) + abs(housex - chickens[k].second);
                     if(house_chicken ==-1||house_chicken>dist) house_chicken = dist;
                 }
@@ -53,7 +51,7 @@ int main(void){
             chickendistance += house_chicken;
         }
         if(ans==-1|ans>chickendistance) ans = chickendistance;
-   }
+   }while(next_permutation(d.begin(), d.end()));
    cout<<ans<<"\n";
     
 }
