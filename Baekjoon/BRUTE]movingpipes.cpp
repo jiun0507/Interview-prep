254d761583be75a5f09651b0b57effe5f70698e1
Jiun Kim <jkim2@bowdoin.edu>
2020-02-24 21:37:04 -0500

    ji
    ji

--- Baekjoon/BRUTE]movingpipes.cpp
+++ Baekjoon/BRUTE]movingpipes.cpp
@@ -1,66 +1,59 @@
-//baekjoon 17070
-//파이프 옮기기 1
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 int board[20][20];
 int n;
-int dy[3] = {0, 1, 1};
-int dx[3] = {1, 0, 1};
+int dy[] = {0, 1, 1};
+int dx[] = {1, 0, 1};
 
 bool check_range(int y, int x){
     return y>=0 && y<n && x>=0 && x<n && board[y][x] == 0;
 }
 
-
 int go(pair<int,int> left, pair<int,int> right){
-    cout<<left.first<<" "<<left.second<<"\n"<<right.first<<" "<<right.second<<"\n\n";
+    // cout<<left.first<<" "<<left.second<<"\n"<<right.first<<" "<<right.second<<"\n\n";
     if(right.first == n-1 && right.second == n-1){
         return 1;
     }
     int ddy = right.first - left.first;
     int ddx = right.second - left.second;
     int ans = 0;
+    bool zero = check_range(right.first + dy[0], right.second + dx[0]);
+    bool one = check_range(right.first + dy[1], right.second + dx[1]);
+    bool two = check_range(right.first + dy[2], right.second + dx[2]);
     if(ddx == 0){ //세로
-        if(check_range(right.first + dy[1], right.second + dx[1]) ){ 
-            pair<int,int> newright = make_pair(right.first + dy[1], right.second + dy[1]);
-             ans += go(right, newright);
-            if(check_range(right.first + dy[2], right.second + dx[2]) && check_range(right.first + dy[0], right.second + dx[0])){
+        if(one){ 
+            pair<int,int> newright = make_pair(right.first + dy[1], right.second + dx[1]);
+            ans += go(right, newright);
+            if(two && zero){
                 newright.second = newright.second + 1;
                 ans += go(right, newright);    
             }
             
         }
-        
-       
     }
-    if(ddy == 0){ //가로
-        if(check_range(right.first + dy[0], right.second + dx[0])){
+    else if(ddy == 0){ //가로
+        if(zero){
             pair<int,int> newright = make_pair(right.first+ dy[0], right.second+dx[0]);
             ans += go(right, newright);
-            if(check_range(right.first + dy[2], right.second + dx[2]) &&check_range(right.first + dy[1], right.second + dx[1])){
-                newright.first++;
+            if(two&&one){
+                newright.first = newright.first + 1;
                 ans += go(right, newright); 
             } 
         } 
         
     }
-    else{
+    else if(ddx == 1 && ddy == 1){
         pair<int,int> newright = make_pair(right.first+ dy[0], right.second+dx[0]);
-        ans= 0;
-        bool one = false;
-        bool two = false;
-        if(check_range(right.first + dy[0], right.second + dx[0])){
+        if(zero){
             ans += go(right, newright);    
-            one = true;
         } 
         newright = make_pair(right.first + dy[1], right.second +dx[1]);
-        if(check_range(right.first + dy[1], right.second + dx[1])){
+        if(one){
             ans += go(right, newright);
-            two = true;
         } 
         newright = make_pair(right.first + dy[2], right.second +dx[2]);   
-        if(check_range(right.first + dy[2], right.second + dx[2]) && one && two){
+        if(one && two && zero){
             ans += go(right, newright);
         }
         
@@ -70,10 +63,13 @@ int go(pair<int,int> left, pair<int,int> right){
 
 int main(void){
     cin>>n;
+    // cout<<n<<"\n";
     for(int i = 0;i<n;i++){
         for(int j = 0;j<n;j++){
             cin>>board[i][j];
+            // cout<<board[i][j]<<" ";
         }
+        // cout<<"\n";
     }
     int ans = go(make_pair(0, 0), make_pair(0, 1));
     cout<<ans<<"\n";
---
+++ Baekjoon/BRUTE]movingpipes2.cpp
@@ -0,0 +1,83 @@
+//baekjoon 17069
+//파이프 옮기기 2
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int board[33][33];
+int n;
+int dy[] = {0, 1, 1};
+int dx[] = {1, 0, 1};
+long long memo[33][33][3];
+
+
+bool check_range(int y, int x){
+    return y>=0 && y<n && x>=0 && x<n && board[y][x] == 0;
+}
+
+long long go(int y, int x, int dir){
+    // cout<<left.first<<" "<<left.second<<"\n"<<right.first<<" "<<right.second<<"\n\n";
+    if(y == n-1 && x == n-1){
+        return 1;
+    }
+    if(memo[y][x][dir] != -1){
+        return memo[y][x][dir];
+    }
+    long long ans = 0;
+    bool zero = check_range(y + dy[0], x + dx[0]);
+    bool one = check_range(y + dy[1], x + dx[1]);
+    bool two = check_range(y + dy[2], x + dx[2]);
+    if(dir == 0){ //세로
+        if(one){ 
+            pair<int,int> newright = make_pair(y + dy[1], x + dx[1]);
+            ans += go(newright.first, newright.second, 0);
+            if(two && zero){
+                newright.second = newright.second + 1;
+                ans += go(newright.first, newright.second, 2);    
+            }
+            
+        }
+    }
+    else if(dir == 1){ //가로
+        if(zero){
+            pair<int,int> newright = make_pair(y+ dy[0], x+dx[0]);
+            ans += go(newright.first, newright.second, 1);
+            if(two&&one){
+                newright.first = newright.first + 1;
+                ans += go(newright.first, newright.second, 2); 
+            } 
+        } 
+        
+    }
+    else if(dir == 2){
+        pair<int,int> newright = make_pair(y+ dy[0], x+dx[0]);
+        if(zero){
+            ans += go(newright.first, newright.second, 1);    
+        } 
+        newright = make_pair(y + dy[1], x +dx[1]);
+        if(one){
+            ans += go(newright.first, newright.second, 0);
+        } 
+        newright = make_pair(y + dy[2], x +dx[2]);   
+        if(one && two && zero){
+            ans += go(newright.first, newright.second, 2);
+        }
+        
+    }
+    memo[y][x][dir] = ans;
+    return ans;
+}
+
+int main(void){
+    cin>>n;
+    // cout<<n<<"\n";
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            cin>>board[i][j];
+            // cout<<board[i][j]<<" ";
+        }
+        // cout<<"\n";
+    }
+    memset(memo, -1, sizeof(memo));
+    long long ans = go(0, 1, 1);
+    cout<<ans<<"\n";
+}
