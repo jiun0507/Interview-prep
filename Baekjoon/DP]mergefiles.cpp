2f9b45aa666a2c147b3df322fa939ee243a6d149
Jiun Kim <jkim2@bowdoin.edu>
2020-03-21 20:26:30 +0900

    ji
    ji

--- Baekjoon/DP]mergefiles.cpp
+++ Baekjoon/DP]mergefiles.cpp
@@ -7,6 +7,23 @@ using namespace std;
 int n;
 vector<int> num;
 int d[501][501];
+int d2[501][501];
+
+int go(int i, int j){
+    if(i == j){
+        return 0;
+    }
+    if(d2[i][j] != -1) return d2[i][j];
+    int sum = num[j];
+    if(i-1>=0) sum -= num[i-1];
+    // cout<<sum<<"\n";
+    int &ans = d2[i][j];
+    for(int k = i;k<j;k++){
+        int temp = go(i, k) + go(k+1, j) + sum;
+        if(ans == -1 || ans > temp) ans = temp;
+    }
+    return ans;
+}
 
 int main(void){
     ios_base::sync_with_stdio(false);
@@ -22,15 +39,12 @@ int main(void){
             cin>>buf;
             num.push_back(buf);
             d[i][i] = buf;
-            // cout<<d[i][i]<<" ";
+            d2[i][i] = buf;
             if(i>0){
                 num[i] += num[i-1];
             }
         }
-        // for(int i = 0 ;i<n;i++){
-        //     cout<<num[i]<<" ";
-        // }
-        // cout<<"\n";
+        cout<<go(0, n-1)<<"\n";
         for(int j = 1;j<n;j++){
             for(int i = 1;i<n;i++){
                 if(i-j<0) continue;
