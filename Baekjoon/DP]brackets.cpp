6f76ad6445c4dd82b489d0bc8d830d652a3f699d
Jiun Kim <jkim2@bowdoin.edu>
2020-03-22 20:57:53 +0900

    ji
    ji

--- Baekjoon/DP]brackets.cpp
+++ Baekjoon/DP]brackets.cpp
@@ -8,6 +8,7 @@ int n;
 long long mod = 1000000007LL;
 int num[5001];
 long long d[5001];
+long long d2[5001][5001];
 
 int main(void){
     ios_base::sync_with_stdio(false);
@@ -18,6 +19,21 @@ int main(void){
         cin>>num[i];
         if(mx<num[i]) mx = num[i];
     }
+    d2[0][0] = 1;
+    for(int i = 1;i<=mx;i+=1){
+        for(int j = 0;j<=i;j++){
+            d2[i][j] += d2[i-1][j+1];
+            d2[i][j] %= mod;
+            if(j!=0){
+                d2[i][j] += d2[i-1][j-1];
+                d2[i][j] %= mod;
+            }
+        }
+    }
+    for(int i = 0;i<n;i++){
+        cout<<d2[num[i]][0]<<"\n";
+    }
+    
     d[2] = 1;
     d[0] = 1;
     for(int i = 4;i<=mx;i+=2){
