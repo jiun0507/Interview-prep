c95db4e0b7ee6857478a7becdec19e433f2d0730
Jiun Kim <jkim2@bowdoin.edu>
2020-04-22 20:05:38 +0900

    ji
    ji

--- Baekjoon/BRUTE]biggesthistogram.cpp
+++ Baekjoon/BRUTE]biggesthistogram.cpp
@@ -6,40 +6,47 @@
 
 using namespace std;
 long long board[100001];
-int main(void){
+int main(void)
+{
     int n;
-    while(cin>>n){
-        if(n == 0) break;
-        for(int i = 0;i<n;i++){
-            cin>>board[i];
+    while (cin >> n)
+    {
+        if (n == 0)
+            break;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> board[i];
         }
         stack<long long> s;
         long long mx = 0;
-        for(int i = 0;i<n;i++){
-            while(!s.empty() && board[s.top()]>board[i]){
+        for (int i = 0; i < n; i++)
+        {
+            while (!s.empty() && board[s.top()] > board[i])
+            {
                 long long length = i;
                 long long height = board[s.top()];
                 s.pop();
-                if(!s.empty()){
-                    length = i-s.top()-1;
+                if (!s.empty())
+                {
+                    length = i - s.top() - 1;
                 }
-                mx = max(mx, height*length);
+                mx = max(mx, height * length);
             }
             mx = max(mx, board[i]);
             s.push(i);
         }
-        while(!s.empty()){
+        while (!s.empty())
+        {
             long long length = n;
             long long height = board[s.top()];
             s.pop();
-            if(!s.empty()){
+            if (!s.empty())
+            {
                 length = length - s.top() - 1;
             }
-            mx = max(mx, height*length);
+            mx = max(mx, height * length);
         }
 
-        cout<<mx<<"\n";
+        cout << mx << "\n";
     }
-
-
 }
