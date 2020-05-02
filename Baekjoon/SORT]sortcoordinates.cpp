da647b81a7fc710e2e321bace9d8032211f2d038
Jiun Kim <jkim2@bowdoin.edu>
2020-05-02 22:13:17 +0900

    ji
    ji

--- Baekjoon/SORT]sortcoordinates.cpp
+++ Baekjoon/SORT]sortcoordinates.cpp
@@ -6,19 +6,22 @@
 
 using namespace std;
 
-int main(void){
+int main(void)
+{
     ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+    cin.tie(nullptr);
     int n;
-    cin>>n;
-    vector<pair<int,int>> a(n);
-    for(int i =0;i<n;i++){
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++)
+    {
         int first, second;
-        cin>>first>>second;
+        cin >> first >> second;
         a[i] = make_pair(first, second);
     }
-	sort(a.begin(), a.end());
-    for(int i =0;i<n;i++){
-        cout<<a[i].first<<" "<<a[i].second<<"\n";
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].first << " " << a[i].second << "\n";
     }
 }
--- Baekjoon/sumofsubarray.cpp
+++ Baekjoon/sumofsubarray.cpp
@@ -4,54 +4,66 @@
 #include <algorithm>
 using namespace std;
 
-
 int n;
 int s;
 int input[20];
 
-bool check(int mask){
+bool check(int mask)
+{
 	int sum = 0;
-	for(int i =0;i<n;i++){
-		if((mask & (1<<i))>0){
+	for (int i = 0; i < n; i++)
+	{
+		if ((mask & (1 << i)) > 0)
+		{
 			sum += input[i];
 		}
 	}
-	if(s == sum){
+	if (s == sum)
+	{
 		return true;
-	} 
-	else{
+	}
+	else
+	{
 		return false;
 	}
 }
 
 //better way ->much simpler
-int masking(){
+int masking()
+{
 	int sum = 0;
-	for(int i = 1; i<(1<<n);i++){
-		if(check(i)) sum += 1;
+	for (int i = 1; i < (1 << n); i++)
+	{
+		if (check(i))
+			sum += 1;
 	}
 	return sum;
 }
 
-int masking2(int index, int mask){
-	if(index == n){
-		if(check(mask)&&mask != 0){
+int masking2(int index, int mask)
+{
+	if (index == n)
+	{
+		if (check(mask) && mask != 0)
+		{
 			//cout<<mask<<"\n";
 			return 1;
 		}
 		return 0;
 	}
 	int ans = masking2(index + 1, mask);
-	mask += 1<<index;
-	ans +=masking2(index + 1, mask);
+	mask += 1 << index;
+	ans += masking2(index + 1, mask);
 	return ans;
 }
 
-int main() {
-	
-	cin>>n>>s;
-	for(int i = 0;i<n;i++){
-		cin>>input[i];
+int main()
+{
+
+	cin >> n >> s;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input[i];
 	}
-	cout<<masking()<<"\n";
+	cout << masking() << "\n";
 }
