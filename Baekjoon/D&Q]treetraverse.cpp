418d8cb65d54a3b4c0605fe122b000db47b8d6b9
Jiun Kim <jkim2@bowdoin.edu>
2020-09-14 22:56:57 +0900

    2263

--- Baekjoon/D&Q]treetraverse.cpp
+++ Baekjoon/D&Q]treetraverse.cpp
@@ -4,32 +4,40 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int inorder[100000], postorder[100000], position[100010];
-void solve(int inx , int iny, int postx, int posty){
-    if(inx>iny || postx>posty) return;
-    cout<<postorder[posty]<<" ";
-    int middle = position[postorder[posty]];
-    int left = middle- 1 -inx;
-    int right = iny - middle;
-    solve(inx, middle-1, postx, postx + left);
-    solve(middle + 1, iny, postx + left + 1, posty-1);
+
+int position[100001];
+int inorder[100000];
+int postorder[100000];
+void preorder(int inorder_start, int inorder_end, int postorder_start, int postorder_end)
+{
+    if (inorder_start > inorder_end || postorder_end < postorder_start)
+    {
+        return;
+    }
+    int root = postorder[postorder_end];
+    int inorder_mid = position[root];
+    int left = inorder_mid - 1 - inorder_start;
+    cout << root << " ";
+    preorder(inorder_start, inorder_mid - 1, postorder_start, postorder_start + left);
+    preorder(inorder_mid + 1, inorder_end, postorder_start + left + 1, postorder_end - 1);
 }
 
-int main(void){
+int main()
+{
     ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	int n;
-	cin>>n;
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> inorder[i];
+        position[inorder[i]] = i;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> postorder[i];
+    }
+    preorder(0, n - 1, 0, n - 1);
 
-    for(int j = 0;j<n;j++){
-        cin>>inorder[j];
-        position[inorder[j]] = j;
-    }
-	for(int j = 0;j<n;j++){
-	    cin>>postorder[j];
-    }
-    
-    solve(0, n-1, 0, n-1);
-    
     return 0;
 }
