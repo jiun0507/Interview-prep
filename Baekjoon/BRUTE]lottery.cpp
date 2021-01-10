739cba3b2ef73e5f3d026c63f67f5f83e5afc112
Jiun Kim <jkim2@bowdoin.edu>
2021-01-10 12:11:54 +0900

    retry

--- Baekjoon/BRUTE]lottery.cpp
+++ Baekjoon/BRUTE]lottery.cpp
@@ -11,38 +11,45 @@ using namespace std;
 
 vector<int> input;
 
-void go(vector<int> &d, int index, int cnt){
-	if(cnt == 6){
-		for(int i = 0;i<6;i++){
-			cout<<d[i]<<" ";
+void go(vector<int> &d, int index, int cnt)
+{
+	if (cnt == 6)
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			cout << d[i] << " ";
 		}
-		cout<<"\n";
+		cout << "\n";
 		return;
 	}
-	if(index >= input.size()) return;
+	if (index >= input.size())
+		return;
 	d.push_back(input[index]);
-	go(d, index+1, cnt+1);
+	go(d, index + 1, cnt + 1);
 	d.pop_back();
 	go(d, index + 1, cnt);
 	return;
 }
 
-int main() {
+int main()
+{
 	int k;
-	cin>>k;
-	while(k != 0){
-		for(int i = 0;i<k;i++){
+	cin >> k;
+	while (k != 0)
+	{
+		for (int i = 0; i < k; i++)
+		{
 			int temp;
-			cin>>temp;
+			cin >> temp;
 			input.push_back(temp);
 		}
 
 		vector<int> d;
 		go(d, 0, 0);
-		cout<<"\n";
+		cout << "\n";
 		input.clear();
-		cin>>k;
+		cin >> k;
 	}
-	
+
 	return 0;
 }
