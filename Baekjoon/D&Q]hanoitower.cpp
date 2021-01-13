708fcdeba9a590b9f0d5ce9e43e88a06db08fa61
Jiun Kim <jkim2@bowdoin.edu>
2021-01-13 20:46:20 +0900

    retry

--- Baekjoon/D&Q]hanoitower.cpp
+++ Baekjoon/D&Q]hanoitower.cpp
@@ -1,27 +1,29 @@
 //baekjoon 11729
 //하노이 탑 이동
+
 #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-
-void solve(int n, int y, int x){
-    int z = 3 - x- y;
-    if(n == 0) return;
-    solve(n-1,y, z);
-    cout<<y+1<<" "<<x+1<<"\n";
-    solve(n-1, z, x);
+void solve(int n, int y, int x)
+{
+    int z = 3 - x - y;
+    if (n == 0)
+        return;
+    solve(n - 1, y, z);
+    cout << y + 1 << " " << x + 1 << "\n";
+    solve(n - 1, z, x);
 }
 
-int main(void){
+int main(void)
+{
     ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	int n;
-    cin>>n;
-    cout<<(1<<n)-1<<"\n";
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << (1 << n) - 1 << "\n";
     solve(3, 0, 2);
-    // cout<<ans.size()<<"\n";
-    
+
     return 0;
 }
