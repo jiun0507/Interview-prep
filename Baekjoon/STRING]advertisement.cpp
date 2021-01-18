abff5842a8276627836540b05531a2ff8e3aa6bc
Jiun Kim <jkim2@bowdoin.edu>
2021-01-18 22:58:45 +0900

    retry

--- Baekjoon/STRING]advertisement.cpp
+++ Baekjoon/STRING]advertisement.cpp
@@ -6,30 +6,37 @@ using namespace std;
 
 int n;
 
-vector<int> preprocess(string s){
+vector<int> preprocess(string s)
+{
     int len = s.size();
     vector<int> pi(len);
     pi[0] = 0;
     int j = 0;
-    for(int i = 1;i < len; i++){
-        while(j>0 && s[i] != s[j]) j = pi[j-1];
-        if(s[i] == s[j]){
-            pi[i] = j+1;
+    for (int i = 1; i < len; i++)
+    {
+        while (j > 0 && s[i] != s[j])
+            j = pi[j - 1];
+        if (s[i] == s[j])
+        {
+            pi[i] = j + 1;
             j++;
-        } else{
+        }
+        else
+        {
             pi[i] = 0;
         }
     }
     return pi;
 }
 
-int main(void){
-    cin>>n;
+int main(void)
+{
+    cin >> n;
     string str;
-    cin>>str;
+    cin >> str;
     vector<int> pi = preprocess(str);
-    int j = n-1;
+    int j = n - 1;
 
-    cout<<n- pi[j]<<"\n";
+    cout << n - pi[j] << "\n";
     return 0;
 }
