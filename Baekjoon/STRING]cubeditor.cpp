fcfcc9c6a5bd0b84f5a7dcaa64695090dcf0551d
Jiun Kim <jkim2@bowdoin.edu>
2021-01-19 15:18:15 +0900

    Update STRING]cubeditor.cpp

--- Baekjoon/STRING]cubeditor.cpp
+++ Baekjoon/STRING]cubeditor.cpp
@@ -1,41 +1,54 @@
+// baekjoon 1701
+// cubeditor
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
 int n;
 
-vector<int> preprocess(string s){
+vector<int> preprocess(string s)
+{
     int len = s.size();
     vector<int> pi(len);
     pi[0] = 0;
     int j = 0;
-    for(int i = 1;i < len; i++){
-        while(j>0 && s[i] != s[j]) j = pi[j-1];
-        if(s[i] == s[j]){
-            pi[i] = j+1;
+    for (int i = 1; i < len; i++)
+    {
+        while (j > 0 && s[i] != s[j])
+            j = pi[j - 1];
+        if (s[i] == s[j])
+        {
+            pi[i] = j + 1;
             j++;
-        } else{
+        }
+        else
+        {
             pi[i] = 0;
         }
     }
     return pi;
 }
 
-int main(void){
+int main(void)
+{
     string str;
-    cin>>str;
+    cin >> str;
     int ans = 0;
     reverse(str.begin(), str.end());
     string a;
-    for(char c : str){
+    for (char c : str)
+    {
         a = c + a;
         vector<int> pi = preprocess(a);
         int len = a.length();
-        for(int i = 0;i<len;i++){ 
-            if(ans<pi[i]) ans = pi[i];
+        for (int i = 0; i < len; i++)
+        {
+            if (ans < pi[i])
+                ans = pi[i];
         }
     }
- 
-    cout<<ans<<"\n";
+
+    cout << ans << "\n";
     return 0;
 }
