803397843267f206b1c906d753690c0237a976f1
Jiun Kim <jkim2@bowdoin.edu>
2021-01-26 21:32:07 +0900

    Update BRUTE]oasisgettogether.cpp

--- Baekjoon/BRUTE]oasisgettogether.cpp
+++ Baekjoon/BRUTE]oasisgettogether.cpp
@@ -5,33 +5,34 @@
 
 using namespace std;
 int people[500001];
-int main(void){
+int main(void)
+{
     int n;
-    cin>>n;
-    for(int i = 0;i<n;i++){
-        cin>>people[i];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> people[i];
     }
-    stack<pair<int,int>> s;
+    stack<pair<int, int>> s;
     long long see = 0;
-    for(int i = 0;i<n;i++){
-        pair<int,int> cur = {people[i], 1};
-        while(!s.empty() && s.top().first <= people[i]){
+    for (int i = 0; i < n; i++)
+    {
+        pair<int, int> cur = {people[i], 1};
+        while (!s.empty() && s.top().first <= people[i])
+        {
             see += s.top().second;
-            if(s.top().first == people[i]){
+            if (s.top().first == people[i])
+            {
                 cur.second += s.top().second;
             }
             s.pop();
-            
         }
-        if(!s.empty()){
+        if (!s.empty())
+        {
             see++;
         }
         s.push(cur);
     }
-        
 
-    cout<<see<<"\n";
-
-
-
+    cout << see << "\n";
 }
