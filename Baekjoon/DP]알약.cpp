54ebe4ec3231dfb8fd1f138d6cf762f2d8343c6c
Jiun Kim <jkim2@bowdoin.edu>
2021-02-13 11:30:40 +0900

    Create DP]알약.cpp

---
+++ Baekjoon/DP]알약.cpp
@@ -0,0 +1,43 @@
+// 4811 알약
+// baekjoon
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+
+long long p[31][31]; // undivided, divided
+// p[i][j] = p[i-1][j+1] + p[i][j-1]
+// p[0][i] = 1 => 남은게 반쪽 알밖에 없으면 1가지 방법밖에 없다.
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    for (int i = 1; i <= 30; i++)
+    {
+        p[0][i] = 1;
+    }
+    for (int i = 1; i <= 30; i++)
+    {
+        for (int j = 0; j <= 30; j++)
+        {
+            p[i][j] = p[i - 1][j + 1];
+            if (j != 0)
+            {
+                p[i][j] += p[i][j - 1];
+            }
+        }
+    }
+    int query;
+    while (true)
+    {
+        cin >> query;
+        if (query == 0)
+            break;
+        cout << p[query][0] << "\n";
+    }
+
+    return 0;
+}
