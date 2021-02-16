72f582be5904b8d1ba5bb08671cfc5e783b58d20
Jiun Kim <jkim2@bowdoin.edu>
2021-02-16 14:22:49 +0900

    Create DP]123더하기7.cpp

---
+++ Baekjoon/DP]123더하기7.cpp
@@ -0,0 +1,41 @@
+// 15992 1,2,3 더하기 7
+// baekjoon
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+
+int p[1001][1001];
+int n, m;
+const int mod = 1000000009;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    p[0][0] = 1;
+    for (int i = 1; i < 1001; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            for (int k = 1; k <= 3; k++)
+            {
+                if (i - k >= 0)
+                {
+                    p[i][j] += p[i - k][j - 1];
+                    p[i][j] %= mod;
+                }
+            }
+        }
+    }
+    while (t--)
+    {
+        cin >> n >> m;
+        cout << p[n][m] << "\n";
+    }
+    return 0;
+}
