ffc06b53e1f30b254a1474d4ab8758ca665a6109
Jiun Kim <jkim2@bowdoin.edu>
2021-09-02 22:03:40 -0400

    Update LEETCODE]trappingrainwater.cpp

--- Baekjoon/LEETCODE]trappingrainwater.cpp
+++ Baekjoon/LEETCODE]trappingrainwater.cpp
@@ -1,23 +1,39 @@
-class Solution {
-public:
-    int trap(vector<int>& height) {
-        stack<int> wall;
-        int water = 0;
-        for(int i = 0;i<height.size();i++){
-            while(!wall.empty() && height[i] > height[wall.top()]){
-                int top = wall.top();
-                wall.pop();
-                int dist = 0;
-                int boundHeight = 0;
-                if(wall.empty()){
-                    break;
-                }
-                dist = i - wall.top() - 1;
-                boundHeight = min(height[i], height[wall.top()]) - height[top];
-                water += dist * boundHeight;
-            }
-            wall.push(i);
+int trap(vector<int>& height)
+{
+    int ans = 0, current = 0;
+    stack<int> st;
+    while (current < height.size()) {
+        while (!st.empty() && height[current] > height[st.top()]) {
+            int top = st.top();
+            st.pop();
+            if (st.empty())
+                break;
+            int distance = current - st.top() - 1;
+            int bounded_height = min(height[current], height[st.top()]) - height[top];
+            ans += distance * bounded_height;
         }
-       return water;
+        st.push(current++);
     }
-};
+    return ans;
+}
+
+int trap(vector<int>& height)
+{
+    if(height.empty())
+        return 0;
+    int ans = 0;
+    int size = height.size();
+    vector<int> left_max(size), right_max(size);
+    left_max[0] = height[0];
+    for (int i = 1; i < size; i++) {
+        left_max[i] = max(height[i], left_max[i - 1]);
+    }
+    right_max[size - 1] = height[size - 1];
+    for (int i = size - 2; i >= 0; i--) {
+        right_max[i] = max(height[i], right_max[i + 1]);
+    }
+    for (int i = 1; i < size - 1; i++) {
+        ans += min(left_max[i], right_max[i]) - height[i];
+    }
+    return ans;
+}
